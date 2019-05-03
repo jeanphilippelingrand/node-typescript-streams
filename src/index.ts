42b58839773cd4136e79c66f665f2137a9fa1bb3
Jean-Philippe Lingrand <35231495+jeanphilippelingrand@users.noreply.github.com>
2019-05-03 13:29:12 -0400

    Update index.ts
    Fix docs

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@ import {Readable} from "./Readable";
 import {Writable} from "./Writable";
 import {Transform} from "./Transform";
 
-// where the data comes from
+// readable: a data producer
 // callback called when data is pushed to the readable intern buffer
 const readable = new Readable(10, function(props) {
     console.log(`R s${props.size} rs${props.rl}`)
 });
 
-// where we transform the data (we group chunks together)
+// transform: data transformer (we group chunks together)
 // callback called when the data is delivered
 const transform = new Transform(10, function(props) {
     console.log(`T rl: ${props.rl} wl: ${props.wl}`);
 });
 
+// writable: a data consumer
 // we act like we write data somewhere and it takes some time
 const writable = new Writable(10, function(props) {
     console.log(`W wl: ${props.wl}`)
